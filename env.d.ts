fa03a2b77d9e21b2220d689d8edce85367e94926
abhikatta <abhinaykatta97@gmail.com>
2024-08-27 11:03:57 +0530

    fix: env fixes

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,9 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         AUTH_SECRET: string;
-        AUTH_GOOGLE_ID: string;
-        AUTH_GOOGLE_SECRET: string;
+        AUTH_GITHUB_ID: string;
+        AUTH_GITHUB_SECRET: string;
         NEXTAUTH_URL: string;
         DATABASE_URL: string;
-        FIREBASE_API_KEY: string;
     }
 }
